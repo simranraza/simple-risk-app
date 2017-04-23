e5c3c23b92b3d9dc19004d58d0dba8ba693ffef5
simranraza <s.imran.raza@gmail.com>
2017-04-23 13:05:30 +1000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './App.css';
+import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <div>Simple Risk App</div>
+      </div>
+    )
+  }
+};
+
+export default App;
